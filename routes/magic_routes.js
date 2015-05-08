f0ff9c9bf39db9107dd8fcb095c15c75d8e2b7bd
Lamson Le <ll217@u.washington.edu>
2015-05-08 15:39:04 -0700

    add write to json for post request

--- routes/magic_routes.js
+++ routes/magic_routes.js
@@ -1,20 +1,29 @@
 'use strict';
 
+var fs = require('fs');
 var bodyparser = require('body-parser');
 
 module.exports = function(router) {
   router.use(bodyparser.json());
 
   router.get('/magic', function(req, res) {
-    res.status(200).json({msg: 'Welcome to the dojo'});
+    res.status(200).json({magic: 'Welcome to the dojo'});
   });
 
-  router.post('/magic', function(req, res) {
-    res.status(202).json({msg: 'Nice casting ' + req.body.msg});
+  router.post('/magic/:spell', function(req, res) {
+    fs.writeFile('./dbjson/' + req.params.spell + '.json', JSON.stringify(req.body), function(err) {
+      if (err) throw err;
+    });
+    res.status(202).json({magic: 'Nice casting ' + req.body.magic});
+
+  });
+
+  router.put('/magic/:spell', function(req, res) {
+    res.status(202).json({magic: 'Recasting ' + req.body.magic});
   });
 
   router.get('*', function(req, res) {
-    res.status(404).json({msg: 'This page is in a black hole'});
+    res.status(404).json({magic: 'This page is in a black hole'});
   });
 
 }
--- test/server_test.js
+++ test/server_test.js
@@ -21,13 +21,25 @@ describe('http server running', function() {
 
   it('should listen to POST request at /magic', function(done) {
     chai.request('localhost:5000')
-      .post('/magic')
-      .send({msg: 'Black'})
+      .post('/magic/missle')
+      .send({magic: 'magic missle'})
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(202);
-        expect(res.body.msg).to.eql('Nice casting Black');
+        expect(res.body.magic).to.eql('Nice casting magic missle');
         done();
       });
   });
+
+  it('should be a 404 page', function(done) {
+    chai.request('localhost:5000')
+      .get('/')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(404);
+        expect(res.body.magic).to.eql('This page is in a black hole');
+        done();
+      });
+  });
+
 });
